c24899172ab32bdf8dffc06e638a1a8c141af9a9
cawfie <sreena4114@gmail.com>
2026-01-24 16:08:14 +0530

    what endgame?  :/

--- src/pages/SelectionPage.jsx
+++ src/pages/SelectionPage.jsx
@@ -64,7 +64,6 @@ const SelectionPage = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      {error && <p className="error-text">{error}</p>}
 
       <div className="button-group">
         <button
